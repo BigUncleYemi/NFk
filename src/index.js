986516998fafe8bcf1d5f175b8b40a383e332939
BigUncleYemi <olayemioyebanji911@gmail.com>
2018-10-19 21:55:52 +0100

    Init commmit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
+import 'popper.js';
+import 'material-design-icons/iconfont/material-icons.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
